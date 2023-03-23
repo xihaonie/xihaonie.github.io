9ffd2bbc026c02922dc3334cac0e85b769020dea
xihaonie <99771683+xihaonie@users.noreply.github.com>
2023-03-24 00:34:39 +0800

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Air Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
+export const SITE_EMAIL = '4444424@gmailcom.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
