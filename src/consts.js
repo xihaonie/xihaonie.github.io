ef43ac4b8f22ca1611a7022162096ecdf6f074a7
xihaonie <99771683+xihaonie@users.noreply.github.com>
2023-03-24 00:37:29 +0800

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Xi Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = '4444424@gmailcom.com'
+export const SITE_EMAIL = '4444424@gmail.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
